04a6ad2ad46e3e61621672d9dd055b6774b6a8fb
Erudite885 <erudite885@gmail.com>
2025-05-27 12:25:41 +0100

    app layout

--- app/(app)/(protected)/_layout.tsx
+++ app/(app)/(protected)/_layout.tsx
@@ -3,54 +3,56 @@ import { Ionicons } from '@expo/vector-icons';
 
 export default function ProtectedLayout() {
   return (
-    <Tabs
-      initialRouteName="wallet"
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ color, size }) => {
-          let iconName: any;
+		<Tabs
+			initialRouteName="wallet"
+			screenOptions={({ route }) => ({
+				tabBarIcon: ({ color, size }) => {
+					let iconName: any;
 
-          switch (route.name) {
-            case 'wallet':
-              iconName = 'wallet-outline';
-              break;
-            case 'buy':
-              iconName = 'cellular-outline';
-              break;
-            case 'index':
-              iconName = 'home-outline';
-              break;
-            case 'history':
-              iconName = 'time-outline';
-              break;
-            case 'settings':
-              iconName = 'settings-outline';
-              break;
-            default:
-              iconName = 'ellipse-outline';
-          }
+					switch (route.name) {
+						case "wallet":
+							iconName = "wallet-outline";
+							break;
+						case "buy":
+							iconName = "cellular-outline";
+							break;
+						case "index":
+							iconName = "home-outline";
+							break;
+						case "history":
+							iconName = "time-outline";
+							break;
+						case "settings":
+							iconName = "settings-outline";
+							break;
+						default:
+							iconName = "ellipse-outline";
+					}
 
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: '#4e9bde',
-        tabBarInactiveTintColor: 'gray',
-        tabBarLabelStyle: {
-          fontSize: 12,
-        },
-        tabBarStyle: {
-          backgroundColor: 'transparent',
-          position: 'absolute',
-          borderTopWidth: 0,
-          elevation: 0, // for Android
-        },
-        headerShown: false,
-      })}
-    >
-      <Tabs.Screen name="wallet" options={{ title: 'Wallet' }} />
-      <Tabs.Screen name="buy" options={{ title: 'Buy Data' }} />
-      <Tabs.Screen name="index" options={{ title: 'Home' }} />
-      <Tabs.Screen name="history" options={{ title: 'History' }} />
-      <Tabs.Screen name="settings" options={{ title: 'Settings' }} />
-      
-    </Tabs>
-  );
+					return <Ionicons name={iconName} size={size} color={color} />;
+				},
+				tabBarActiveTintColor: "#4e9bde",
+				tabBarInactiveTintColor: "gray",
+				tabBarLabelStyle: {
+					fontSize: 12,
+				},
+				tabBarStyle: {
+					backgroundColor: "transparent",
+					position: "absolute",
+					borderTopWidth: 0,
+					elevation: 0, // for Android
+				},
+				headerShown: false,
+			})}
+		>
+			<Tabs.Screen name="wallet" options={{ title: "Wallet" }} />
+			<Tabs.Screen name="buy" options={{ title: "Buy Data" }} />
+			<Tabs.Screen name="index" options={{ title: "Home" }} />
+			<Tabs.Screen name="history" options={{ title: "History" }} />
+			<Tabs.Screen
+				name="settings"
+				options={{ title: "Settings", tabBarStyle: { display: "none" } }}
+			/>
+		</Tabs>
+	);
 }
