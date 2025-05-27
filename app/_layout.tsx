97e81331f6fb809a12a7904ce3df4a9debc64923
Erudite885 <erudite885@gmail.com>
2025-05-27 12:12:46 +0100

    tanstackquery added

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { Slot } from "expo-router";
 import "react-native-reanimated";
@@ -7,25 +7,25 @@ import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { SupabaseProvider, useSupabase } from "@/context/supabase-provider";
 
 export default function RootLayout() {
-	// const [queryClient] = useState(
-	// 	() =>
-	// 		new QueryClient({
-	// 			defaultOptions: {
-	// 				queries: {
-	// 					refetchOnWindowFocus: false,
-	// 					staleTime: 5 * 60 * 1000,
-	// 				},
-	// 			},
-	// 		}),
-	// );
+	const [queryClient] = useState(
+		() =>
+			new QueryClient({
+				defaultOptions: {
+					queries: {
+						refetchOnWindowFocus: false,
+						staleTime: 5 * 60 * 1000,
+					},
+				},
+			}),
+	);
 
 	return (
 		<GestureHandlerRootView style={{ flex: 1 }}>
-			{/* <QueryClientProvider client={queryClient}> */}
+			<QueryClientProvider client={queryClient}>
 				<SupabaseProvider>
 					<Slot />
 				</SupabaseProvider>
-			{/* </QueryClientProvider> */}
+			</QueryClientProvider>
 		</GestureHandlerRootView>
 	);
 }
