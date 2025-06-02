395aa9cf8447e9cdab11fd31eeeb83a3aba031a0
Erudite885 <erudite885@gmail.com>
2025-06-02 01:00:14 +0100

    queryclient modified

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,23 +1,25 @@
-import React, { useState, useEffect } from "react";
+import { SupabaseProvider } from "@/context/supabase-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import { Slot } from "expo-router";
-import "react-native-reanimated";
+import React, { useState } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
-import { SupabaseProvider, useSupabase } from "@/context/supabase-provider";
+import "react-native-reanimated";
 
 export default function RootLayout() {
-	const [queryClient] = useState(
-		() =>
-			new QueryClient({
-				defaultOptions: {
-					queries: {
-						refetchOnWindowFocus: false,
-						staleTime: 5 * 60 * 1000,
-					},
-				},
-			}),
-	);
+	// const [queryClient] = useState(
+	// 	() =>
+	// 		new QueryClient({
+	// 			defaultOptions: {
+	// 				queries: {
+	// 					refetchOnWindowFocus: false,
+	// 					staleTime: 5 * 60 * 1000,
+	// 				},
+	// 			},
+	// 		}),
+	// );
+
+	const queryClient = new QueryClient();
 
 	return (
 		<GestureHandlerRootView style={{ flex: 1 }}>
