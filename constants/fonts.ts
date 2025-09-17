1c76aacc9fc235c7e30e2630c2e5ea5d7a986817
Erudite885 <erudite885@gmail.com>
2025-09-17 11:40:09 +0100

    removed export of unsued fonts

--- constants/fonts.ts
+++ constants/fonts.ts
@@ -1,8 +1,4 @@
 // constants/fonts.ts
 export const fonts = {
   default: "Roboto-Regular",
-  // openSans: "OpenSans-Regular",
-  // cursive: "Cursive-Regular",
-  // handlee: "Handlee-Regular",
-  // notoSans: "NotoSans-Regular",
 };
